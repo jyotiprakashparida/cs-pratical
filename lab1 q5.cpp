f9eb8158cc858a0ccc4d3e607109e44c5ebd3173
jyotiprakashparida <31472189+jyotiprakashparida@users.noreply.github.com>
2017-09-01 10:36:20 +0530

    Create lab1 q5.cpp

---
+++ lab1 q5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+float length,breadth,area;
+cout<< "Enter the length of rectangle=";
+cin>>length;
+cout<< "Enter the breadth of rectangle=";
+cin>>breadth;
+area=(length*breadth);
+cout<<"area of rectangle ="<<area;
+	return 0;
+}
