f3dcdb828c27e568fa26ebfe7d9871111b1c441c
jyotiprakashparida <31472189+jyotiprakashparida@users.noreply.github.com>
2017-09-01 10:34:27 +0530

    Create lab1 q4.cpp

---
+++ lab1 q4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	float length,breadth,perimeter;
+cout<< "Enter the length of rectangle=";
+cin>>length;
+cout<< "Enter the breadth of rectangle=";
+cin>>breadth;
+perimeter=2*(length+breadth);
+cout<<"perimeter of rectangle ="<<perimeter;
+	return 0;
+}
