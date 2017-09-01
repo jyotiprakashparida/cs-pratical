1b34a2b72f2b977f61ad812b69209a69bfaab361
jyotiprakashparida <31472189+jyotiprakashparida@users.noreply.github.com>
2017-09-01 10:38:06 +0530

    Create lab1 q6.cpp

---
+++ lab1 q6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+float r,d,p,a;
+cout<<"enter the radius of the circle=";
+cin>>r;
+cout<<"diameter of the circle=";
+d=2*r;
+cout<<"diameter of the circle="<<d;
+cout<<"perimeter of the circle=";
+p=2*3.14159265358979*r;
+cout<<"perimeter of the circle="<<p;
+cout<<"area of the circle=";
+a=3.14159265358979*r*r;
+cout<<"area of the circle="<<a;
+	return 0;
+}
