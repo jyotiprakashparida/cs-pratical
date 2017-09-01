8cf39dca2ecf2edbffd4150a8d8cfe014f69836c
jyotiprakashparida <31472189+jyotiprakashparida@users.noreply.github.com>
2017-09-01 10:25:42 +0530

    Create lab1 q1.cpp

---
+++ lab1 q1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{string name="";
+int age=0;
+
+cout<<"enter your name: \n";
+cin>>name;
+
+cout<<"enyer your age:";
+cin>>age;
+
+cout<<"your name is: " <<name<<".\nyour age is: " << age <<".";
+return 0;
