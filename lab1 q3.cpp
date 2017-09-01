b0e633d7f92bf65ac3b75fb2f71bf0be398847ae
jyotiprakashparida <31472189+jyotiprakashparida@users.noreply.github.com>
2017-09-01 10:33:03 +0530

    Create lab1 q3.cpp

---
+++ lab1 q3.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	float x,y,s,d,m,d1;
+	 cout<<"enter two numbers:";
+	 cin>>x>>y;
+	 cout<<"add the numbers";
+	 s=x+y;
+	 cout<<"sum of these two numbers="<<s; 
+	 cout<<"substract the second number from the first number";
+	 d=x-y;
+	 cout<<"difference of these two numbers="<<d;
+	  cout<<"multiply the numbers";
+	 m=x*y;
+	 cout<<"multiplication of these two numbers="<<m; 
+	  cout<<"divide the first number by the second number";
+	 d1=x/y;
+	 cout<<"division of these two numbers="<<d1;
+	return 0;
+}
